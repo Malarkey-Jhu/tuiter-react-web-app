71924099354de8082107a5f089f1273e334c93a5
Alan Jhu <alan61109@gmail.com>
2023-02-18 13:48:51 +0800

    a6

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {useLocation} from "react-router";
+import cx from "classnames";
 
 const NavigationSidebar = () => {
  const {pathname} = useLocation();
@@ -11,14 +12,22 @@ const NavigationSidebar = () => {
     <>
     <ul className="list-group">
       <li className="list-group-item"><i className="fab fa-2x fa-tumblr-square"></i></li>
-      <li className={`list-group-item ${active == 'home' ? 'active' : '' }`}><i className="fas fa-home"></i>
+      <li className={
+        cx("list-group-item", {
+          "active": active == 'home'
+          })
+      }><i className="fas fa-home"></i>
         <Link to={'/tuiter/home'}>
         <span className="d-sm-none d-xl-inline-block">
           Home
         </span>
         </Link>
       </li>
-      <li className={`list-group-item ${active == 'explore' ? 'active' : '' }`}><i className="fas fa-hashtag"></i>
+      <li className={
+        cx("list-group-item", {
+          "active": active == 'explore'
+          })
+        }><i className="fas fa-hashtag"></i>
         <Link to={'/tuiter/explore'}>
         <span className="d-sm-none d-xl-inline-block">
           Explore
